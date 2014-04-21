321d4f89423ad2a4e4bc02b4bea35017b02adf3f
AYaKomarov <a.ya.komarov@gmail.com>
2014-04-21 17:42:25 +0400

    change Get

--- Get/getRawNodes.js
+++ Get/getRawNodes.js
@@ -1,3 +1,4 @@
+
 function showVisit(visitItem) {
                  
             document.write("<li>" + visitItem.id + "</li>");
@@ -10,89 +11,124 @@ function showVisit(visitItem) {
             document.write("</ul>");
 }
   
-function showHist(histItems) {
+function showHist(histItem) {
                      
-            document.write("<li>" + histItems.title + "</li>");
+            document.write("<li>" + histItem.title + "</li>");
             
             document.write("<ul>");
-            document.write("<li>" + histItems.id + "</li>");
-            document.write("<li>" + histItems.url + "</li>");
-            document.write("<li>" + histItems.visitCount + "</li>");
-            document.write("<li>" + histItems.typedCount + "</li>");
+            document.write("<li>" + histItem.id + "</li>");
+            document.write("<li>" + histItem.url + "</li>");
+            document.write("<li>" + histItem.visitCount + "</li>");
+            document.write("<li>" + histItem.typedCount + "</li>");
             document.write("</ul>");
             
 }
+
+function showRawNodes(array) {
+
+   for(var i=0; i< array.length ;i++) {
+      showHist( array[i].HistoryItem )
+      showVisit(array[i].VisitItem)
+      document.write("<hr>");                  
+   }
+}
       
+      
+function Get(text_, startTime_, endTime_, maxResults_) {
+   
+   //constructor
+   var text = text_
+   var startTime = startTime_
+   var endTime = endTime_
+   var maxResults = maxResults_
+   
+   
+   //public
+   this.applyFunction = function ( main_callback ) {
+   
+      getHistoryItems( curry_snd( getVisitItems, curry_snd( sortRawNodes, main_callback )  )  );
+   }
+   
+
+   //private             
+   function getHistoryItems( callback ) {
+      
+      chrome.history.search({
+               'text': text,
+               'startTime': startTime,
+               'endTime': endTime,
+               'maxResults': maxResults       
+               },
+               callback
+      );       
+   }
 
 
-  function foo2( visit_items, hist_item, startTime, endTime) {
-       
-       for(var j=0; j < visit_items.length; j++) {
+
+   function getVisitItems( array, callback ) {
+      
+         async.concat
+         (
+               array, 
                
-               if( startTime <= visit_items[j].visitTime && visit_items[j].visitTime <= endTime ) {    
-                  /* 
-                  showHist( hist_item )
-                  showVisit(visit_items[j])
-                  document.write("<hr>");
-                  */
-                  var rawNode = { histItem: hist_item, visitItem: visit_items[j] }
-                  rawNodes.push(rawNode)
-               } 
-       }      
-       
-  }
-  
-  
-function foo1 ( hist_items, startTime, endTime ) {
+               function(item, doneCallback ) 
+               {
+               
+                   chrome.history.getVisits( 
+                                    {
+                                     'url' : item.url 
+                                    }
+                                    
+                                   , function (arrVisitItems) 
+                                      {
+                                          arrRawNodes = []
+                                          for(var i=0; i < arrVisitItems.length; i++) {
+                                             
+                                             if( startTime <= arrVisitItems[i].visitTime && arrVisitItems[i].visitTime <= endTime ) {  // where startTime and endTime ?
+                                                
+                                                arrRawNodes.push(  { HistoryItem : item , VisitItem : arrVisitItems[i] }  )   // this.item ?
+                                             }
+                                          }
+                                          doneCallback(null, arrRawNodes )
+                                      }
+                   );
+               },
+               
+               function(err, results) 
+               {
+                  callback(results)
+               }   
+          )      
+   }
 
-   function curry_foo2( hi, st, et ) {
-      return function(a) {
-         return foo2(a, hi, st, et );
-      }
+
+
+   function sortRawNodes (array, callback) {
+         
+         async.sortBy
+         (
+               array, 
+               
+               function(item, doneCallback ) 
+               {
+                   doneCallback(null, item.VisitItem.visitTime )
+               },
+               
+               function(err, results) 
+               {
+                  callback(results)
+               }   
+          )  
    }
+
+
+   function curry_snd ( function_with_two_args , snd_arg ) {
       
-   for(var i=0; i < hist_items.length; i++) {
-      
-      //var A = { hi : hist_items[i] };
-      var url = hist_items[i].url 
-      
-      var par;
-      chrome.history.getVisits( {
-                                'url' :url 
-                                }
-                                , curry_foo2(hist_items[i], startTime, endTime)
-      );
-      
+      return function(fst_arg) { function_with_two_args (fst_arg, snd_arg) }
    }
-  
-}     
-      
-      
-      
-function Get(text, startTime, endTime, maxResults) {
-   
-   function curry_foo1( st, et ) {
-      return function(a) {
-         return foo1(a, st, et);
-      }
-   }
-    
-   chrome.history.search({
-            'text': text,
-            'startTime': startTime,
-            'endTime': endTime,
-            'maxResults': maxResults       
-            },
-            curry_foo1(startTime, endTime)
-   );     
-   
+
+
 }
 
          
-                     
-//        
-var rawNodes = [];            
-Get('', 0, 1397471396974.2898, 3);
-
-alert("Get raw nodes")
-
+                    
---
+++ Get/testGet.js
@@ -0,0 +1,2 @@
+get = new Get('', 0, 139747139697400, 3) 
+get.applyFunction(showRawNodes)
