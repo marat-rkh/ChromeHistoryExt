eae3fbd3e6a7cdf9228f811d5514f605e47844da
AYaKomarov <a.ya.komarov@gmail.com>
2014-04-21 18:19:50 +0400

    change files

--- Get/testGet.js
+++ Get/testGet.js
@@ -1,2 +1,39 @@
+
+function showVisit(visitItem) {
+                 
+            document.write("<li>" + visitItem.id + "</li>");
+            
+            document.write("<ul>");
+            document.write("<li>" + visitItem.visitId + "</li>");
+            document.write("<li>" + visitItem.visitTime + "</li>");
+            document.write("<li>" + visitItem.referringVisitId + "</li>");
+            document.write("<li>" + visitItem.transition+ "</li>");
+            document.write("</ul>");
+}
+  
+function showHist(histItem) {
+                     
+            document.write("<li>" + histItem.title + "</li>");
+            
+            document.write("<ul>");
+            document.write("<li>" + histItem.id + "</li>");
+            document.write("<li>" + histItem.url + "</li>");
+            document.write("<li>" + histItem.visitCount + "</li>");
+            document.write("<li>" + histItem.typedCount + "</li>");
+            document.write("</ul>");
+            
+}
+
+function showRawNodes(array) {
+
+   for(var i=0; i< array.length ;i++) {
+      showHist( array[i].HistoryItem )
+      showVisit(array[i].VisitItem)
+      document.write("<hr>");                  
+   }
+}
+
+
+
 get = new Get('', 0, 139747139697400, 3) 
 get.applyFunction(showRawNodes)
