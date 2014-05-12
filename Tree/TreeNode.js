f12681f5a6c24eb7bedf771f3d4846433fe96c75
AYaKomarov <a.ya.komarov@gmail.com>
2014-05-12 15:31:11 +0400

    add getDate

--- Tree/TreeNode.js
+++ Tree/TreeNode.js
@@ -11,5 +11,26 @@ function TreeNode (rawNode_, parent_) {
     this.equals = function (treeNode) {
         return this.id === treeNode.id;
     }
-
+    
+    this.getDate = function() {
+      
+      floorTime = Math.floor( this.rawNode.VisitItem.visitTime );
+      sec = (floorTime - floorTime % 1000) / 1000;
+      var date = new Date(0); 
+      date.setUTCSeconds(sec);
+      
+      hours = date.getHours().toString();
+      minutes = date.getMinutes().toString();
+      seconds = date.getSeconds().toString();
+      
+      if(minutes.length == 1) {
+        minutes = "0" + minutes;
+      }
+      
+      if(seconds.length == 1) {
+        seconds = "0" + seconds;
+      }
+      
+      return hours + ":" + minutes + ":" + seconds;
+    }
 }
