8d2af8755e3c4d93b8a786418b24914bd594acbb
AYaKomarov <a.ya.komarov@gmail.com>
2014-05-22 21:39:20 +0400

    fix ForestBuilder and add clear button (test version)

--- Tree/ForestBuilder.js
+++ Tree/ForestBuilder.js
@@ -1,31 +1,41 @@
 
 function ForestBuilder( rawNodes ) {
  
+   //chrome.history.deleteAll(function(){});  window.localStorage.clear();
+   
+   function findParentIdForReloadItems(startId) {
+      
+      var curRealoadVisitId = startId;
+      while(curRealoadVisitId in redirectTempMap) {
+         curRealoadVisitId = redirectTempMap[curRealoadVisitId];
+      }
+      return curRealoadVisitId;
+   }
+   
    var arrayTreeNodes = [];
    var visitIdToTreeNodeMap = {};
    var redirectTempMap = {};
    var roots = [];
-  
+   
+   
    for(var i=0; i < rawNodes.length; i++) {
       
       var currentTreeNode = null;
       var curVisId = rawNodes[i].VisitItem.visitId; 
       var refVisId = window.localStorage.getItem(curVisId);
       
-      if( rawNodes[i].VisitItem.transitionType == 'reload' &&  window.localStorage.getItem(rawNodes[i].VisitItem.referringVisitId) !== null ) {
-         redirectTempMap[rawNodes[i].VisitItem.visitId] = window.localStorage.getItem(rawNodes[i].VisitItem.referringVisitId);
-         
+      if( rawNodes[i].VisitItem.transition == 'reload' &&  
+          ( window.localStorage.getItem(curVisId) !== null &&
+            window.localStorage.getItem(curVisId) !== "null")) {
+          
+         redirectTempMap[curVisId] = window.localStorage.getItem(curVisId); 
          var time = rawNodes[i].VisitItem.visitTime;
-         var curRealoadVisitId = rawNodes[i].VisitItem.visitId;
-         while(redirectTempMap[curRealoadVisitId] !== null) {
-            curRealoadVisitId = redirectTempMap[curRealoadVisitId];
-         }
-         visitIdToTreeNodeMap[curRealoadVisitId].rawNode.VisitItem.visitTime = time;
-         
+         var parentIdForReloadItems = findParentIdForReloadItems(curVisId);
+         visitIdToTreeNodeMap[parentIdForReloadItems].rawNode.VisitItem.visitTime = time;
          continue;
       }
       
-      if( window.localStorage.getItem(curVisId) == null ) {   
+      if( window.localStorage.getItem(curVisId) == null || window.localStorage.getItem(curVisId) == "null" ) {   
          refVisId = rawNodes[i].VisitItem.referringVisitId;      
       }
 
@@ -35,12 +45,9 @@ function ForestBuilder( rawNodes ) {
          visitIdToTreeNodeMap[refVisId].childrenArray.push( currentTreeNode );
          
       } else if (refVisId in redirectTempMap) {
-         var curRealoadVisitId = refVisId;
-         while(redirectTempMap[curRealoadVisitId] !== null) {
-            curRealoadVisitId = redirectTempMap[curRealoadVisitId];
-         }
-         currentTreeNode = new TreeNode( rawNodes[i], visitIdToTreeNodeMap[curRealoadVisitId] );
-         visitIdToTreeNodeMap[curRealoadVisitId].childrenArray.push( currentTreeNode );
+         var parentIdForReloadItems = findParentIdForReloadItems(refVisId);
+         currentTreeNode = new TreeNode( rawNodes[i], visitIdToTreeNodeMap[parentIdForReloadItems] );
+         visitIdToTreeNodeMap[parentIdForReloadItems].childrenArray.push( currentTreeNode );
          
       } else {
          currentTreeNode = new TreeNode(rawNodes[i], null);
