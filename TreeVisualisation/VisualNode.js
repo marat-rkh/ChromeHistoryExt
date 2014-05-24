f685c6be0f200f4e3b93e1bccdba960b25721249
maratx387 <maratx387@gmail.com>
2014-05-24 16:45:33 +0400

    Vertical edges have been changed but they don't work correctly

--- TreeVisualisation/VisualNode.js
+++ TreeVisualisation/VisualNode.js
@@ -31,11 +31,11 @@ function VisualNode (/*htmlRepr | id, childrenList, isVisible*/) {
     this.getHtml = function () {
         return htmlNode;
     };
-    
-    this.getLIChildren = function() {
-      return getChildrenContainer().children;  
-    };
-    
+//
+//    this.getLIChildren = function() {
+//      return getChildrenContainer().children;
+//    };
+//
     this.getChildren = function () {
         var childrenInHtml = getChildrenContainer().children;
         var visualChildren = [];
